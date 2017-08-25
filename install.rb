edfbd51384fed3f22918ae7e0aa43a09789842bf
Isaac Sloan <isaac@isaacsloan.com>
2017-08-25 00:30:42 -0600

    better installer

--- install.rb
+++ install.rb
@@ -1,8 +1,24 @@
 dotfilepath = File.join(File.expand_path(File.dirname(__FILE__)), "dot")
 simlinkpath = ENV["HOME"] || Dir.pwd
 dotfilelist = Dir.entries(dotfilepath).select{|f| !%w(. ..).include?(f)}
-backupdir = simlinkpath+"/dotfilebackup_"+Time.now.strftime("%Y%m%d%H%M%S")
+backupdir = File.join(simlinkpath, "dotfilebackup_"+Time.now.strftime("%Y%m%d%H%M%S"))
 Dir.mkdir(backupdir)
-dotfilelist.each{|f|  `mv #{simlinkpath}/.#{f} #{backupdir}/`}
-dotfilelist.each{|f| `ln -fs #{dotfilepath}/#{f} #{simlinkpath}/.#{f}`}
+puts "##################################{(0...backupdir.size).map{"#"}.join}"
+puts "Backing up existing dotfiles to #{backupdir}."
+puts "##################################{(0...backupdir.size).map{"#"}.join}"
+dotfilelist.each do |f|
+  if File.exists?(filename = "#{simlinkpath}/.#{f}")
+    puts filename 
+    `mv #{filename} #{backupdir}/`
+  end
+end
+puts "\n########################"
+puts "Installing new dotfiles."
+puts "########################"
+dotfilelist.each do |f|
+  puts f
+  `ln -fs #{dotfilepath}/#{f} #{simlinkpath}/.#{f}`
+end
+puts "\nInstalling Fonts..."
 `cp #{dotfilepath}/../fonts/* ~/Library/Fonts/`
+puts "\nEnjoy the awesomeness!"
